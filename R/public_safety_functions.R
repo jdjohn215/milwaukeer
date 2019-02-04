186e55554edaf705166d54760e876f41fe5852d0
John D. Johnson <jdjohn215@gmail.com>
2019-02-04 11:01:07 -0600

    Add filters and documentation to get_ems()

--- R/public_safety_functions.R
+++ R/public_safety_functions.R
@@ -322,11 +322,38 @@ get_FireIncidents <- function(start_date, end_date, include_missingDate, spatial
   date.filtered
 }
 
+
+#' Retrieve the Emergency Calls for Service (EMS) dataset
+#'
+#' \code{get_ems} returns a data.frame containing a list of fire dept incidents
+#'   for the requested time period (if specified).
+#'
+#'  No geocoding is available as the only geographies provided are zip codes and aldermanic districts. Refer to the data dictionary for variable descriptions:
+#'   \url{https://data.milwaukee.gov/dataset/mfdems}
+#'
+#' @param start_date The first date to be included. Must be coercible to class Date.
+#' Defaults to first date available.
+#' @param end_date The last date to be included. Must be coercible to class Date.
+#' Defaults to last date available.
+#' @param include_missingDate Logical. If TRUE values without a valid date will be included. Defaults to FALSE.
+#' @return A dataframe.
+#'
+#' @examples
+#' get_ems()
+#' get_ems(start_date = as.Date("2018-01-01"), end_date = as.Date("2018-02-01"))
+
 # Get emergency medical services trips
-get_ems <- function(start_year, end_year) {
+get_ems <- function(start_date, end_date, include_missingDate) {
+  if(missing(start_date)){
+    start_date <- as.Date("2015-01-01")
+  }
+  if(missing(end_date)){
+    end_date <- Sys.Date()
+  }
+
   ckanr_setup(url = "https://data.milwaukee.gov")
   start <- Sys.time()
-  years <- start_year:end_year
+  years <- as.numeric(stringr::str_sub(start_date, 1, 4)):as.numeric(stringr::str_sub(end_date, 1, 4))
   df.list <- list()
   if(is.element(2015, years)){
     res15 <- resource_show(id = "9ab724f8-2e43-420e-9c86-e757acacb412", as = "table")
@@ -357,6 +384,28 @@ get_ems <- function(start_year, end_year) {
   end <- Sys.time()
   fetchTime <- difftime(end, start, units = "secs")
   print(paste("Download time:", round(fetchTime, 2), "seconds."))
-  raw
+
+  # filter out missing dates
+  raw2 <- raw %>%
+    filter(!is.na(Incident.Date))
+
+  date.filtered <- raw2 %>%
+    filter(Incident.Date >= as.Date(start_date),
+           Incident.Date <= as.Date(end_date))
+
+  missing.date <- length(raw$Case.Number) - length(raw2$Case.Number)
+  print(paste(missing.date, "Incidents are missing properly formatted dates.",
+              "Set include_missingDate = TRUE to include them."))
+
+  if(missing(include_missingDate)){
+    include_missingDate = FALSE
+  }
+
+  if(include_missingDate == TRUE){
+    no.date <- anti_join(raw2, raw3)
+    date.filtered <- bind_rows(date.filtered, no.date)
+  }
+
+  date.filtered
 }
 
