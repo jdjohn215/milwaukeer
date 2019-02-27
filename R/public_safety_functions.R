27e5124c3b052f5280018d9ceccf58b8b227272e
John D. Johnson <jdjohn215@gmail.com>
2019-02-27 16:38:54 -0600

    add past years to function and update arguments

--- R/public_safety_functions.R
+++ R/public_safety_functions.R
@@ -249,7 +249,6 @@ get_TrafficAccidents <- function(start_date, end_date, include_missingDate) {
 #' Defaults to first date available.
 #' @param end_date The last date to be included. Must be coercible to class Date.
 #' Defaults to last date available.
-#' @param include_missingDate Logical. If TRUE values without a valid date will be included. Defaults to FALSE.
 #' @param make_spatial Logical. If TRUE the output is class sf. Defaults to FALSE.
 #' @param shape An object of class sf. If included, the output will be filtered using
 #' st_intersection
@@ -266,77 +265,110 @@ get_TrafficAccidents <- function(start_date, end_date, include_missingDate) {
 #' get_FireIncidents(start_date = as.Date("2018-01-01"), end_date = as.Date("2018-02-01"))
 
 # Get Milwaukee Fire Department actions
-get_FireIncidents <- function(start_date, end_date, include_missingDate, spatial, shape) {
+get_FireIncidents <- function(start_date, end_date, spatial, shape) {
   # Make spatial have default of false
   if(missing(spatial)){
     spatial = FALSE
   }
+  if(missing(start_date)){
+    start_date = as.Date("2015-01-01")
+  }
+  if(missing(end_date)){
+    end_date = as.Date(Sys.Date())
+  }
 
-  ckanr_setup(url = "https://data.milwaukee.gov")
-  res <- resource_show(id = "ed310d17-2a6d-4334-9102-ff20f4462743", as = "table")
+  years.list <- as.numeric(stringr::str_sub(start_date, 1, 4)):as.numeric(stringr::str_sub(end_date, 1, 4))
+  raw.list <- list()
+
   start <- Sys.time()
-  raw <- fetch(res$url)
+  # download 2015
+  if(2015 %in% years.list == TRUE){
+    raw2015 <- readr::read_csv("https://data.milwaukee.gov/dataset/b61a89f7-1604-4b3a-b465-42f5a1e232bc/resource/97509bea-dfa4-435e-aa75-5499adf7bbe0/download/og_2015_primary.csv",
+                               col_types = readr::cols(.default = "c")) %>%
+      filter(`Case Number` != "Case Number") %>%
+      tidyr::separate(col = "Date Received", into = c("DateReceived", "TimeReceived"),
+                      sep = " ") %>%
+      tidyr::separate(col = "DateReceived", into = c("month", "day", "year"),
+                      sep = "/") %>%
+      mutate(DateReceived = paste(year, month, day, sep = "-")) %>%
+      select(-year, -month, -day)
+    raw.list[[length(raw.list)+1]] <- raw2015
+  }
+  if(2016 %in% years.list == TRUE){
+    raw2016 <- readr::read_csv("https://data.milwaukee.gov/dataset/b61a89f7-1604-4b3a-b465-42f5a1e232bc/resource/5d2d6ce8-e4f6-4c41-a800-31e4eface37f/download/og_2016_primary.csv",
+                               col_types = readr::cols(.default = "c")) %>%
+      filter(`Case Number` != "Case Number") %>%
+      tidyr::separate(col = "Date Received", into = c("DateReceived", "TimeReceived"),
+                      sep = " ") %>%
+      tidyr::separate(col = "DateReceived", into = c("month", "day", "year"),
+                      sep = "/") %>%
+      mutate(DateReceived = paste(year, month, day, sep = "-")) %>%
+      select(-year, -month, -day)
+    raw.list[[length(raw.list)+1]] <- raw2016
+  }
+  if(2017 %in% years.list == TRUE){
+    raw2017 <- readr::read_csv("https://data.milwaukee.gov/dataset/b61a89f7-1604-4b3a-b465-42f5a1e232bc/resource/25a3fb9e-161c-416e-82da-6837c704b643/download/og_2017_primary.csv",
+                               col_types = readr::cols(.default = "c")) %>%
+      filter(`Case Number` != "Case Number") %>%
+      tidyr::separate(col = "Date Received", into = c("DateReceived", "TimeReceived"),
+                      sep = " ") %>%
+      tidyr::separate(col = "DateReceived", into = c("month", "day", "year"),
+                      sep = "/") %>%
+      mutate(DateReceived = paste(year, month, day, sep = "-")) %>%
+      select(-year, -month, -day)
+    raw.list[[length(raw.list)+1]] <- raw2017
+  }
+  if(2018 %in% years.list == TRUE){
+    raw2018 <- readr::read_csv("https://data.milwaukee.gov/dataset/c62f2c78-da2e-48d8-9215-37fb11c143f1/resource/ed310d17-2a6d-4334-9102-ff20f4462743/download/mfdprimary.csv",
+                               col_types = readr::cols(.default = "c")) %>%
+      filter(`Case Number` != "Case Number") %>%
+      tidyr::separate(col = "Date Received", into = c("DateReceived", "TimeReceived"),
+                      sep = " ")
+    raw.list[[length(raw.list)+1]] <- raw2018
+  }
+
+  raw <- bind_rows(raw.list)
+
   end <- Sys.time()
   fetchTime <- difftime(end, start, units = "secs")
   print(paste("Download time:", round(fetchTime, 2), "seconds."))
 
+  # fix variable names
+  names(raw) <- stringr::str_replace_all(names(raw), " ", "")
+
   # Split datetime into two variables
   raw2 <- raw %>%
-    tidyr::separate(col = "Date.Received", sep = " ", into = c("DateReceived", "TimeReceived")) %>%
-    mutate(date = as.Date(DateReceived))
+    mutate(DateReceived = as.Date(DateReceived))
 
   # filter out missing dates
   raw3 <- raw2 %>%
     filter(!is.na(DateReceived))
 
-  # Filter by date and time, default to full range of data
-  if(missing(start_date)){
-    start_date <- min(raw3$DateReceived)
-  }
-  if(missing(end_date)){
-    end_date <- max(raw3$DateReceived)
-  }
-
   date.filtered <- raw3 %>%
     filter(DateReceived >= as.Date(start_date),
            DateReceived <= as.Date(end_date))
+  d.output <- date.filtered
 
-  missing.date <- length(raw2$Case.Number) - length(raw3$Case.Number)
-  print(paste(missing.date, "Incidents are missing properly formatted dates.",
-              "Set include_missingDate = TRUE to include them."))
-
-  if(missing(include_missingDate)){
-    include_missingDate = FALSE
-  }
-
-  if(include_missingDate == TRUE){
-    no.date <- anti_join(raw2, raw3)
-    date.filtered <- bind_rows(date.filtered, no.date)
-  }
-
-  # Intersect if shape is present
-  if(!missing(shape)){
-    shape <- st_transform(shape, crs = 32054)
-
+  # make spatial
+  if(!missing(shape) | spatial == TRUE){
     date.filtered <- st_as_sf(date.filtered, coords = c("Longitude", "Latitude"),
-                     crs = 4326) %>%
-      st_transform(crs = st_crs(shape))
-    date.filtered <- st_intersection(shape, date.filtered)
+                              crs = 4326)
 
-    # make class(data.frame) if spatial == F
-    if(spatial == FALSE){
+    # filter by shape
+    if(!missing(shape)){
       date.filtered <- date.filtered %>%
-        st_set_geometry(NULL)
+        st_transform(crs = st_crs(shape)) %>%
+        st_intersection(shape)
     }
+
+    # remove spatial attributes if spatial == FALSE
+    if(spatial == FALSE){
+      date.filtered <- st_set_geometry(date.filtered, NULL)
+    }
+    d.output <- date.filtered
   }
 
-  # Make date.filtered spatial if spatial==T and missing(shape)
-  if(spatial == TRUE & missing(shape)){
-    date.filtered <- st_as_sf(date.filtered, coords = c("Latitude", "Longitude"),
-                              crs = 32054)
-  }
-
-  date.filtered
+  d.output
 }
 
 
